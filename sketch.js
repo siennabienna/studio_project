e1d0f3a6cce47242fb0e75d6356b33943b41bc31
Sienna <sienna.lee.burns@gmail.com>
2025-02-18 05:02:45 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,6 +8,16 @@ let titanicIcon;
 let titanicBackground;
 let backstreetBoys;
 let startButton;
+let taskbar;
+let date;
+let internetIcon;
+let internetBackground;
+let catPictures = [];
+let usenetIcon;
+let usenetBackground;
+let windowsFont;
+let codeIcon;
+let codeBackgrouond;
 
 // variables for where the windows are on screen
 let paintWindowX = 120;
@@ -20,25 +30,54 @@ let titanicX = 60;
 let titanicY = 420;
 let titanicWidth = 300;
 let titanicHeight = 300 * (538/730);
+let internetX = 0;
+let internetY = 60;
+let internetWidth = 650;
+let internetHeight = 650 * (577/962);
+let usenetX = 0;
+let usenetY = 60;
+let usenetWidth = 500;
+let usenetHeight = 500 * (621/628);
+let codeX = 0;
+let codeY = 0;
+let codeWidth = 500;
+let codeHeight = 500 * (649/823);
 
 // variables to tell if a window is open
 let isPaintOpen = false;
 let isCDPlayerOpen = false;
 let isTitanicPlayerOpen = false;
+let isInternetOpen = false;
+let isUsenetOpen = false;
+let isCodeOpen = false;
 
 function preload() {
   backgroundImg = loadImage("bliss.jpg");
   startButton = loadImage("start.png");
+  taskbar = loadImage("taskbar.png");
+  date = loadImage("date.png");
   paintIcon = loadImage("ms_paint.png");
   paintFrameImg = loadImage("paintBoarder.jpg");
   cdPlayerIcon = loadImage("cd_player.png");
   cdPlayerBackground = loadImage("cdPlayerBackground.png");
   titanicIcon = loadImage("titanicIcon.png");
   titanicBackground = loadImage("titanicPlayer.png");
+  internetIcon = loadImage("internet.png");
+  internetBackground = loadImage("internetBackground.png");
+  usenetIcon = loadImage("usenet.png");
+  usenetBackground = loadImage("usenetWindow.png");
+  windowsFont = loadFont("Windows Regular.ttf");
+  codeIcon = loadImage("studioProject.png");
+  codeBackgrouond = loadImage("codeScreen.png");
+
+  catPictures.push("Keyboard_cat.jpg");
+  catPictures.push("mehcat.jpg");
+  catPictures.push("catbacon.jpg");
 }
 
 function setup() {
   pixelDensity(1);
+  textFont(windowsFont);
   backstreetBoys = loadSound("minden_road.m4a");
 
   createCanvas(windowWidth, windowHeight);
@@ -56,7 +95,9 @@ function draw() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   cdPlayerX = windowWidth - cdPlayerWidth - 10;
-  resetDesktop();
+  usenetX = windowWidth - usenetWidth - 10;
+  usenetY = windowHeight - usenetHeight - 90;
+  refreshDesktop();
 }
 
 // variables for all the buttons in each window
@@ -71,6 +112,20 @@ let cdPlayerStop;
 let titanicBtn;
 let closeTitanicBtn;
 let hourglassImg;
+
+let internetBtn;
+let closeInternetBtn;
+let virusButton;
+
+let usenetBtn;
+let closeUsenetBtn;
+let messageInput;
+let sendButton;
+
+let codeBtn;
+let closeCodeBtn;
+let codeDisplay;
+let codeScreen;
 
 function addButtons() {
   // paint icon
@@ -91,6 +146,24 @@ function addButtons() {
   titanicBtn.size(100, 100);
   titanicBtn.hide();
   titanicBtn.mousePressed(openTitanicPlayer);
+  // internet icon
+  internetBtn = createImg("void.png");
+  internetBtn.position(0, 100);
+  internetBtn.size(100, 100);
+  internetBtn.hide();
+  internetBtn.mousePressed(openInternet);
+  // usenet icon
+  usenetBtn = createImg("void.png");
+  usenetBtn.position(100, 100);
+  usenetBtn.size(100, 100);
+  usenetBtn.hide();
+  usenetBtn.mousePressed(openUsenet);
+  // code icon
+  codeBtn = createImg("void.png");
+  codeBtn.position(0, 300);
+  codeBtn.size(100, 100);
+  codeBtn.hide();
+  codeBtn.mousePressed(openCode);
 
   // paint window
   redSlider = createSlider(0, 255);
@@ -132,6 +205,49 @@ function addButtons() {
   closeTitanicBtn.size(25, 25);
   hourglassImg.size(80, 80);
   closeTitanicBtn.mousePressed(closeTitanicPlayer);
+
+  // internet window
+  closeInternetBtn = createImg("void.png");
+  virusButton = createImg("void.png");
+  closeInternetBtn.hide();
+  virusButton.hide();
+  closeInternetBtn.position(internetX + internetWidth - 25, internetY);
+  virusButton.position(internetX + 200, internetY + 270);
+  closeInternetBtn.size(25, 25);
+  virusButton.size(250, 50);
+  virusButton.mousePressed(showCats);
+  closeInternetBtn.mousePressed(closeInternet);
+
+  // usenet window
+  closeUsenetBtn = createImg("void.png");
+  closeUsenetBtn.hide();
+  closeUsenetBtn.position(usenetX + usenetWidth - 25, usenetY);
+  closeUsenetBtn.size(25, 25);
+  closeUsenetBtn.mousePressed(closeUsenet);
+  messageInput = createInput();
+  messageInput.position(usenetX + 35, usenetY + 440);
+  messageInput.style("color", "white");
+  messageInput.style("backgroundColor", "transparent");
+  messageInput.size(350, 20);
+  messageInput.hide();
+  sendButton = createButton("Send");
+  sendButton.size(50, 25);
+  sendButton.position(usenetX + 35 + 360, usenetY + 440);
+  sendButton.hide();
+  sendButton.mousePressed(addMessageToForum);
+
+  // code window
+  closeCodeBtn = createImg("void.png");
+  closeCodeBtn.hide();
+  closeCodeBtn.position(codeX + codeWidth - 25, codeY);
+  closeCodeBtn.size(25, 25);
+  closeCodeBtn.mousePressed(closeCode);
+  codeScreen = createSpan(addButtons);
+  codeScreen.position(codeX + 20, codeY + 63);
+  codeScreen.size(374, 230);
+  codeScreen.style("overflow-y", "scroll");
+  codeScreen.style("text-wrap", "pretty");
+  codeScreen.hide();
 }
 
 // for every window, i have a function that opens the window (shows the buttons),
@@ -141,6 +257,9 @@ function openDesktop() {
   paintBtn.show();
   cdPlayerBtn.show();
   titanicBtn.show();
+  internetBtn.show();
+  usenetBtn.show();
+  codeBtn.show();
 
   drawDesktop();
 }
@@ -150,10 +269,15 @@ function drawDesktop() {
   image(paintIcon, 0, 0,100,100);
   image(cdPlayerIcon,100, 0,100,100);
   image(titanicIcon,200, 0,100,100);
-  image(startButton, 0, windowHeight - 85);
+  image(taskbar, 0, windowHeight - 40, windowWidth, 40);
+  image(startButton, 0, windowHeight - 40, 40 * (58/28), 40);
+  image(date, windowWidth - 40 * (86/28), windowHeight - 40, 40 * (86/28), 40);
+  image(internetIcon, 0, 100, 100, 100);
+  image(usenetIcon, 100, 100, 100, 100);
+  image(codeIcon, 0, 300, 100, 100);
 }
 
-function resetDesktop() {
+function refreshDesktop() {
   drawDesktop();
 
   if(isPaintOpen)
@@ -162,6 +286,99 @@ function resetDesktop() {
     drawCDPlayer();
   if(isTitanicPlayerOpen)
     drawTitanicPlayer();
+  if(isInternetOpen)
+    drawInternet();
+  if(isUsenetOpen)
+    drawUsenet();
+  if(isCodeOpen)
+    drawCode();
+}
+
+// code for code
+
+function openCode() {
+  isCodeOpen = true;
+  closeCodeBtn.show();
+  codeScreen.show();
+  drawCode();
+}
+
+function drawCode() {
+  image(codeBackgrouond, codeX, codeY, codeWidth, codeHeight);
+}
+
+function closeCode() {
+  isCodeOpen = false;
+  closeCodeBtn.hide();
+  codeScreen.hide();
+  refreshDesktop();
+}
+
+// code for usenet
+
+function openUsenet() {
+  isUsenetOpen = true;
+  closeUsenetBtn.show();
+  messageInput.show();
+  sendButton.show();
+  drawUsenet();
+}
+
+let messages = [];
+function drawUsenet() {
+  image(usenetBackground, usenetX, usenetY, usenetWidth, usenetHeight);
+  
+  textSize(12);
+  fill(255);
+  for(let i = 0; i < messages.length; i++) {
+    text("> " + messages[i], usenetX + 35, usenetY + 269 + i * 15);
+  }
+  text('Type Your Response... ', usenetX + 35, usenetY + 430);
+}
+
+function addMessageToForum() {
+  messages.push(messageInput.value());
+  messageInput.value("");
+  refreshDesktop();
+}
+
+function closeUsenet() {
+  closeUsenetBtn.hide();
+  messageInput.hide();
+  sendButton.hide();
+  isUsenetOpen = false;
+  refreshDesktop();
+}
+
+// code for the internet
+
+function openInternet() {
+  isInternetOpen = true;
+  closeInternetBtn.show();
+  virusButton.show();
+  drawInternet();
+}
+
+function drawInternet() {
+  image(internetBackground, internetX, internetY, internetWidth, internetHeight);
+}
+
+function closeInternet() {
+  closeInternetBtn.hide();
+  virusButton.hide();
+  isInternetOpen = false;
+  refreshDesktop();
+}
+
+function showCats() {
+  for(let i = 0; i < 10; i++) {
+    let img = createImg(random(catPictures));
+
+    img.position(random(0, windowWidth - 400), random(0, windowHeight - 400));
+    img.mousePressed(() => {
+      img.remove();
+    });
+  }
 }
 
 // code for the titanic player window
@@ -181,7 +398,7 @@ function closeTitanicPlayer() {
   closeTitanicBtn.hide();
   hourglassImg.hide();
   isTitanicPlayerOpen = false;
-  resetDesktop();
+  refreshDesktop();
 }
 
 // code for the cd player window
@@ -201,8 +418,9 @@ function closeCDPlayer() {
   closeCdPlayerBtn.hide();
   cdPlayerPlay.hide();
   cdPlayerStop.hide();
+  backstreetBoys.stop();
   isCDPlayerOpen = false;
-  resetDesktop();
+  refreshDesktop();
 }
 
 function playMusic() {
@@ -268,7 +486,7 @@ function closePaintWindow() {
   greenSlider.hide();
   blueSlider.hide();
   isPaintOpen = false;
-  resetDesktop();
+  refreshDesktop();
 }
 
 function pixelsPaint() {
